9cbdf43be158060395efe07c67f99f804756edb6
Cielio Castro <hamtarf2@gmail.com>
2023-10-20 20:25:22 -0300

    start js code

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert('hi')
+const form = document.getElementById('form-activity');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const inputActivityName = document.getElementById('activity-name');
+    const inputActivityNote = document.getElementById('activity-note');
+
+    alert(`Atividade ${inputActivityName.value} - Nota ${inputActivityNote.value}`);
+})
