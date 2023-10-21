7b45c315ddec7a0bc0839c5d9d673e069ea91fd2
Cielio Castro <hamtarf2@gmail.com>
2023-10-20 22:49:42 -0300

    form edit

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 const form = document.getElementById('form-activity');
+const imgApproved = '<img src="./images/aprovado.png" alt="emoji celebrando"'
+const imgDisapproved = '<img src="./images/reprovado.png" alt="emoji decepcionado"'
+
+let lines = '';
+
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     const inputActivityName = document.getElementById('activity-name');
     const inputActivityNote = document.getElementById('activity-note');
 
-    alert(`Atividade ${inputActivityName.value} - Nota ${inputActivityNote.value}`);
-})
+
+    let theLine = '<tr>';
+    theLine += `<td>${inputActivityName.value}</td>`;
+    theLine += `<td>${inputActivityNote.value}</td>`;
+    theLine += `<td>${inputActivityNote.value >= 7 ? imgApproved : imgDisapproved}</td>`;
+    theLine += `<tr>`;
+
+    lines += theLine;
+
+    const bodyTable = document.querySelector('tbody');
+    bodyTable.innerHTML = lines;
+
+    inputActivityName.value = '';
+    inputActivityNote.value = '';
+});
